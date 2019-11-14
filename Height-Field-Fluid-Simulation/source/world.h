2506217575af2bfa2b71bd6b384a755b2c10ef72
Marcus <marcus.wallin93@outlook.com>
2019-11-14 11:16:25 +0100

    added world file, commit before everything crashes

---
+++ Height-Field-Fluid-Simulation/source/world.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef WATERMASS_H
+#define WATERMASS_H
+
+#include "MicroGlut.h"
+
+
+//#include "GL_utilities.h"
+#include "VectorUtils3.h"
+
+
+
+#endif
--- Height-Field-Fluid-Simulation/source/world_generator.cpp
+++ Height-Field-Fluid-Simulation/source/world_generator.cpp
@@ -3,21 +3,18 @@
 
 
 
-
-
+//generates the terrain model based on texture data
+//should change to height array data
 Model* GenerateTerrain(TextureData* tex)
 {
 	const int vertexCount = tex->width * tex->height;
 	int triangleCount = (tex->width - 1) * (tex->height - 1) * 2;
 	int x, z;
-	GLfloat* vertexArray;
 
-	//vertexArray = new GLfloat[vertexCount];//(GLfloat*)malloc(sizeof(GLfloat) * 3 * vertexCount);
-	vertexArray = (GLfloat*)malloc(sizeof(GLfloat) * 3 * vertexCount);
-
-	GLfloat* normalArray = (GLfloat*)malloc(sizeof(GLfloat) * 3 * vertexCount);
-	GLfloat* texCoordArray = (GLfloat*)malloc(sizeof(GLfloat) * 2 * vertexCount);
-	GLuint* indexArray = (GLuint*)malloc(sizeof(GLuint) * triangleCount * 3);
+    GLfloat* vertexArray = new GLfloat[3*vertexCount];
+	GLfloat* normalArray = new GLfloat[3 * vertexCount];
+	GLfloat* texCoordArray = new GLfloat[2 * vertexCount];
+	GLuint* indexArray = new GLuint[3 * triangleCount];
 
 
 	printf("bpp %d\n", tex->bpp);
@@ -28,17 +25,14 @@ Model* GenerateTerrain(TextureData* tex)
 			vertexArray[(x + z * tex->width) * 3 + 0] = x / 1.0;
 			vertexArray[(x + z * tex->width) * 3 + 1] = tex->imageData[(x + z * tex->width) * (tex->bpp / 8)] / 20.0;
 			vertexArray[(x + z * tex->width) * 3 + 2] = z / 1.0;
-			// Normal vectors. You need to calculate these.
 
 
-			// Texture coordinates. You may want to scale them.
 			texCoordArray[(x + z * tex->width) * 2 + 0] = x; // (float)x / tex->width;
 			texCoordArray[(x + z * tex->width) * 2 + 1] = z; // (float)z / tex->height;
 		}
 	for (x = 0; x < tex->width; x++)
 		for (z = 0; z < tex->height; z++)
 		{
-
 			vec3 normal = calculateNormal(vertexArray, x, z, tex);
 
 			normalArray[(x + z * tex->width) * 3 + 0] = normal.x;
@@ -61,7 +55,6 @@ Model* GenerateTerrain(TextureData* tex)
 	// End of terrain generation
 
 	// Create Model and upload to GPU:
-
 	Model* model = LoadDataToModel(
 		vertexArray,
 		normalArray,
